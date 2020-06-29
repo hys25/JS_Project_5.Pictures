cd4eae50e0be34028116f3b46bedf4076ea3e26b
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-06-29 22:29:50 +0300

    Loading elements from the server

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -17,15 +17,6 @@ const forms = () => {
               question: 'assets/question.php'
           };
       
-          const postData = async (url, data) => {
-              let res = await fetch(url, {
-                  method: "POST",
-                  body: data
-              });
-      
-              return await res.text();
-          };
-      
           const clearInputs = () => {
               inputs.forEach(item => {
                   item.value = '';
